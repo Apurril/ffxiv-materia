62b3f020e642962bdc4fceb9b9665740e45c102b
April <April@Rain>
2020-02-17 20:30:59 +0000

    Base

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+
+const Root = () => <App />;
+
+ReactDOM.render(<Root />, document.getElementById("root"));
