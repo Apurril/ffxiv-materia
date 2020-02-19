5b35a896306a46ce31a14f31200d2ccddca50ad5
April <April@Rain>
2020-02-19 13:42:21 +0000

    pointless code

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-const Root = () => <App />;
-
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
